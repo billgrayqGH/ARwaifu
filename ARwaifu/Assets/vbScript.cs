144031fdb907227754010958da8eed0b4c56e7be
billgrayqGH <bgquitalig@yahoo.com>
2018-02-05 08:31:35 -0500

    AR Waifu Project with Vuforia and Unity
    AR Unity Project

---
+++ ARwaifu/Assets/vbScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject vbButtonObject;
+	private GameObject zombie;
+	// Use this for initialization
+	void Start () {
+
+		vbButtonObject = GameObject.Find ("actionButton");
+		zombie = GameObject.Find ("zombie");
+		vbButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+
+	}
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+
+		Debug.Log("button down!!!");
+		zombie.GetComponent<Animation> ().Play();
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+
+		Debug.Log ("button released");
+		zombie.GetComponent<Animation> ().Stop();
+	}
+
+}
